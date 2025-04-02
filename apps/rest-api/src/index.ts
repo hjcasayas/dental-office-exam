0a5c4efbf08ff897f73e88922abae9d2a2f4002a
hjcasayas <henlyjade.casayas@gmail.com>
2025-04-02 17:29:11 +0000

    feat: use the user in memory db implementations

--- apps/rest-api/src/index.ts
+++ apps/rest-api/src/index.ts
@@ -1,11 +1,28 @@
-import { server as expressServer } from '@dental/implementations';
+import {
+  addUserServiceImpl,
+  server as expressServer,
+  getUserByEmailServiceImpl,
+} from '@/implementations';
+import {
+  registerUseCase,
+  getUserByEmailService,
+  addUserService,
+} from '@dental/features';
 import { createServer } from 'node:http';
 
 main();
 
 async function main() {
   const PORT = process.env.PORT ?? 8080;
-  const server = createServer(expressServer);
+
+  const server = createServer(
+    expressServer({
+      registerUseCase: registerUseCase({
+        getUserByEmailService: getUserByEmailService(getUserByEmailServiceImpl),
+        addUserService: addUserService(addUserServiceImpl),
+      }),
+    })
+  );
 
   server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);

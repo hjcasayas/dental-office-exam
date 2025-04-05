2f272f4d25aebe9f260a79c09d9817635d03e037
hjcasayas <henlyjade.casayas@gmail.com>
2025-04-05 12:45:56 +0000

    feat: added zod parse schema implementation

--- packages/implementations/src/zod/parse-schema-zod.service.ts
+++ packages/implementations/src/zod/parse-schema-zod.service.ts
@@ -1,16 +1,36 @@
-import type { ParseSchemaService } from '@dental/features/src/utilities/index.js';
-import { Schema, ZodError } from 'zod';
+import {
+  ValidationError,
+  type ParseSchemaService,
+  type SerializedError,
+} from '@dental/features/src/utilities/index.js';
+import { Schema } from 'zod';
 
 export const parseSchemaZodService =
-  <TInput, TOutput>(
-    zodSchema: Schema<TOutput>
-  ): ParseSchemaService<TInput, TOutput, ZodError> =>
-  (config: TInput) => {
-    const { success, data, error } = zodSchema.safeParse(config);
+  <TInput>(zodSchema: Schema<TInput>): ParseSchemaService<TInput> =>
+  (input: TInput) => {
+    const { success, data, error } = zodSchema.safeParse(input);
 
-    if (success) {
-      return { success, data };
+    if (success && data != null) {
+      return { success, data, error: null };
     }
 
-    return { success, error };
+    if (!success && error != null) {
+      const serializeErrors: SerializedError[] = [];
+
+      const errorEntries = Object.entries(error.flatten().fieldErrors);
+      errorEntries.forEach((error) => {
+        serializeErrors.push({
+          field: error[0],
+          message: (error[1] as string[]).join(', '),
+        });
+      });
+
+      return {
+        success,
+        data: null,
+        error: new ValidationError(serializeErrors),
+      };
+    }
+
+    return { success, data: null, error: null };
   };

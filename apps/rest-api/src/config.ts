0c5a9bce90eb19747c64dbad4637424afa15242d
hjcasayas <henlyjade.casayas@gmail.com>
2025-04-05 02:26:09 +0000

    refactor: renamed config interface file

--- apps/rest-api/src/config.ts
+++
@@ -1,8 +0,0 @@
-export interface EnvConfig {
-  nodeEnv?: string;
-  port: number;
-  mongoUsername: string;
-  mongoPassword: string;
-  mongoConnectionString: string;
-  mongoDB: string;
-}
---
+++ apps/rest-api/src/config.type.ts
@@ -0,0 +1,8 @@
+export interface EnvConfig {
+  mongoConnectionString: string;
+  mongoPassword: string;
+  mongoUsername: string;
+  nodeEnv?: string;
+  mongoDB: string;
+  port: number;
+}

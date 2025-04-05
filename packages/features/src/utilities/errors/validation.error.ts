a09f67514d9c0db0e558e6acc8ddb6e4194b9ee5
hjcasayas <henlyjade.casayas@gmail.com>
2025-04-05 07:12:34 +0000

    feat: validation error extends bad request error

---
+++ packages/features/src/utilities/errors/validation.error.ts
@@ -0,0 +1,9 @@
+import { BadRequestError } from '../index.js';
+import type { SerializedError } from './serialized-error.type.js';
+
+export class ValidationError extends BadRequestError {
+  constructor(errors?: SerializedError[], message?: string) {
+    super(errors ?? [], message ?? 'Validation Error');
+    Object.setPrototypeOf(this, ValidationError.prototype);
+  }
+}

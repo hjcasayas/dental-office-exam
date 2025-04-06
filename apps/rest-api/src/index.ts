6a416342f3498ae0437f52e527e3f1e33057f092
hjcasayas <henlyjade.casayas@gmail.com>
2025-04-06 01:12:58 +0000

    feat: use the express server with its dependencies

--- apps/rest-api/src/index.ts
+++ apps/rest-api/src/index.ts
@@ -6,8 +6,10 @@ import {
 } from 'node:http';
 import {
   environments,
+  registerUseCase,
   ValidationError,
   type Environment,
+  type RegisterUseCase,
 } from '@dental/features';
 import {
   envConfigSchema,
@@ -19,7 +21,17 @@ import {
   createWinstonLogger,
 } from '@dental/implementations/winston';
 
+import { server as expressServer } from '@dental/implementations/express';
+
+import { registerUseCaseParamsSchema } from '@dental/implementations/zod';
+import { bcryptHashPasswordService } from '@dental/implementations/bcrypt';
+
 import type { EnvConfig } from './config.type.js';
+import {
+  inMemoryGetUserByEmailService,
+  inMemoryDB,
+  inMemoryAddUserService,
+} from '@dental/implementations/in-memory';
 
 let server: Server<
   typeof IncomingMessage,
@@ -60,7 +72,15 @@ const log = winstonLoggerService(
   })
 );
 
-server = createServer();
+const register: RegisterUseCase = registerUseCase({
+  parseParamsSchema: parseSchemaZodService(registerUseCaseParamsSchema),
+  getUserByEmail: inMemoryGetUserByEmailService(inMemoryDB),
+  hashPassword: bcryptHashPasswordService(),
+  addUser: inMemoryAddUserService(inMemoryDB),
+  log,
+});
+
+server = createServer(expressServer({ register }));
 
 server.listen(port, () => {
   log('info', `Server is listening on port ${port}`);

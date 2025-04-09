d0eeaed45b6345c14bf4b52b240dd01f29118786
hjcasayas <henlyjade.casayas@gmail.com>
2025-04-09 19:14:57 +0000

    feat: added result types to parse schema service

--- packages/features/src/utilities/schema/parse-schema.service.ts
+++ packages/features/src/utilities/schema/parse-schema.service.ts
@@ -1,10 +1,16 @@
 import type { ValidationError } from '../errors/index.js';
 
-export interface ParseSchemaServiceResult<TData> {
-  success: boolean;
-  data?: TData | null;
-  error?: ValidationError | null;
+export interface ParseSchemaServiceSuccessResult<TData> {
+  success: true;
+  data: TData;
 }
+export interface ParseSchemaServiceErrorResult {
+  success: false;
+  error: ValidationError;
+}
+export type ParseSchemaServiceResult<TData> =
+  | ParseSchemaServiceSuccessResult<TData>
+  | ParseSchemaServiceErrorResult;
 
 export type ParseSchemaService<TInput> = (
   input: TInput

1a519fe3163202e783903c9fb9e8f3557ed323aa
hjcasayas <henlyjade.casayas@gmail.com>
2025-04-09 16:57:50 +0000

    refactor: added StatusKey type and move files contents to index

--- packages/features/src/utilities/status-codes/index.ts
+++ packages/features/src/utilities/status-codes/index.ts
@@ -1,2 +1,23 @@
-export * from './reason-phrases.js';
-export * from './status-codes.js';
+export type StatusKey = keyof typeof StatusCodes;
+
+export const StatusCodes = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  TOO_MANY_REQUESTS: 429,
+  INTERNAL_SERVER_ERROR: 500,
+} as const;
+
+export const ReasonPhrases: Record<StatusKey, string> = {
+  OK: 'OK',
+  CREATED: 'Created',
+  BAD_REQUEST: 'Bad Request',
+  UNAUTHORIZED: 'Unauthorized',
+  FORBIDDEN: 'Forbidden',
+  NOT_FOUND: 'Not Found',
+  TOO_MANY_REQUESTS: 'Too Many Requests',
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
+} as const;

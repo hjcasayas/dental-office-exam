e64c7ae1910e1e78406426bb61be2c161b40623a
hjcasayas <henlyjade.casayas@gmail.com>
2025-04-10 07:21:50 +0000

    fix: remove validation error

--- packages/features/src/utilities/errors/validation.error.ts
+++
@@ -1,9 +0,0 @@
-import { BadRequestError } from '../index.js';
-import type { SerializedError } from './serialized-error.type.js';
-
-export class ValidationError extends BadRequestError {
-  constructor(errors?: SerializedError[]) {
-    super(errors);
-    Object.setPrototypeOf(this, ValidationError.prototype);
-  }
-}

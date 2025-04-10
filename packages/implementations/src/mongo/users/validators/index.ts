27b916822c168b456551bbc7df402c8e63d8bfbe
hjcasayas <henlyjade.casayas@gmail.com>
2025-04-10 07:28:48 +0000

    refactor: return boolean instead of error instance

--- packages/implementations/src/mongo/users/validators/index.ts
+++ packages/implementations/src/mongo/users/validators/index.ts
@@ -1,14 +1,8 @@
 import { Schema } from 'zod';
 
-import { serializedZodError } from '@dental/implementations/zod';
-import { ValidationError } from '@dental/features';
-
 export const validator =
   <TInput>(schema: Schema) =>
   (input: TInput) => {
-    const { success, error } = schema.safeParse(input);
-
-    if (!success || error != null) {
-      throw new ValidationError(serializedZodError(error));
-    }
+    const { success } = schema.safeParse(input);
+    return success;
   };
